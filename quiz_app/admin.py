3a6d2faf9d5847d19fb836d16c6091bd0084118e
KKrntzk <sophie.kornetzki@gmail.com>
2026-08-10 13:37:09 +0200

    feat(quiz): add admin panel for quizzes and questions

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz_app.models import Quiz, Question
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 0
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ["title", "owner", "created_at"]
+    search_fields = ["title", "description"]
+    list_filter = ["created_at"]
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_title", "quiz", "answer"]
+    search_fields = ["question_title"]
+    list_filter = ["created_at"]
