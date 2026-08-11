92f09d0330b9cc8f484fca2811c344c97b6acf11
KKrntzk <sophie.kornetzki@gmail.com>
2026-08-11 12:50:52 +0200

    docs(auth): add docstrings to cookie authentication

--- auth_app/authentication.py
+++ auth_app/authentication.py
@@ -2,7 +2,10 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class CookieJWTAuthentication(JWTAuthentication):
+    """Authenticates users via the JWT stored in the access_token cookie."""
+
     def authenticate(self, request):
+        """Return the user and token from the access_token cookie, or None."""
         access_token = request.COOKIES.get("access_token")
 
         if access_token is None:
