6d4ab104b6143004925eb0f39f22780ec2d074fd
KKrntzk <sophie.kornetzki@gmail.com>
2026-08-11 12:41:42 +0200

    docs(auth): add docstrings and tidy imports in serializers

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,9 +1,11 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    """Validates registration data and creates a new user."""
+
     confirmed_password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -14,6 +16,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         }
 
     def validate(self, data):
+        """Ensure the password and its confirmation match."""
         if data["password"] != data["confirmed_password"]:
             raise serializers.ValidationError(
                 {"confirmed_password": "Passwords do not match."}
@@ -21,6 +24,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
+        """Create a user with a hashed password."""
         validated_data.pop("confirmed_password")
         user = User.objects.create_user(
             username=validated_data["username"],
@@ -31,7 +35,10 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(TokenObtainPairSerializer):
+    """Extends token login with basic user data in the response."""
+
     def validate(self, attrs):
+        """Validate credentials and attach user data to the result."""
         data = super().validate(attrs)
         data["user"] = {
             "id": self.user.id,
